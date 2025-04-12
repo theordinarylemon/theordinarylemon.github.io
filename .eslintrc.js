6c7382fa49f04431158086fb1bc52f3163e8f4e0
theordinarylemon <wyccaii@163.com>
2025-04-12 21:25:51 +0800

    CI test4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-    parser: 'vue-eslint-parser'
+    root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended', // 比 vue3-essential 更严格的规则集
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false // 避免需要 babel 配置文件
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off' // 可选：关闭组件名多单词警告
+  }
 }
